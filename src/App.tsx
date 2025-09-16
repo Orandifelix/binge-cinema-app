029fb79107e3707f6206ee54ed9341c09e1286ae
Orandifelix <felixorandi@gmail.com>
2025-09-17 02:53:19 +0300

    chore: initial commit with Vite + React + TS + Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-blue-400">
+        🚀 Tailwind 4 is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
