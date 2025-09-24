32a6f44bae08ddcada4e36fcc48993057efc8e9e
Orandifelix <felixorandi@gmail.com>
2025-09-24 16:01:45 +0300

    main page responsiveness for small screens

--- src/assets/components/Pages/Mainpage.tsx
+++ src/assets/components/Pages/Mainpage.tsx
@@ -6,21 +6,26 @@ import Navbar from "../Navbar"
 
 const Mainpage = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-900 text-gray-900 font-sans">
+    <div className="flex flex-col min-h-screen bg-gray-900 text-gray-100 font-sans">
       {/* Navbar */}
-      <div className="px-18">
-      <Navbar />
-     </div>
+      <div className="px-4 sm:px-6 lg:px-12 xl:px-20">
+        <Navbar />
+      </div>
+
       {/* Main content */}
-      <main className="flex-1  overflow-y-auto">
+      <main className="flex-1 overflow-y-auto">
         <Landing />
-        <div className="px-18 bg-gray-950">
-        <Home />
+
+        {/* Home Section */}
+        <div className="px-4 sm:px-6 lg:px-12 xl:px-20 bg-gray-950">
+          <Home />
         </div>
       </main>
+
+      {/* Footer */}
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Mainpage
+export default Mainpage;
